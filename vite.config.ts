38c8b34b55935661d676db1c5764727eaf36c631
Khari Johnson <kj@gvempire.com>
2023-02-28 00:43:42 -0600

    feat(ci): implement gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://kuhree.github.io/searchspring-demo",
   define: {
     "import.meta.vitest": "undefined",
   },
